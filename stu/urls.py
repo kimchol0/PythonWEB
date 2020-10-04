ad555307be4e93519977fa0919dd466860ec58ad
KimChol <kimchol2ece@outlook.com>
2020-10-04 14:49:43 +0800

    GET方式实现的简单登录演示功能

--- stu/urls.py
+++ stu/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.login_view)
+    url(r'^$', views.index_view),
+    url(r'^login/', views.login_view)
 ]
--- stu/views.py
+++ stu/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 # Create your views here.
-def login_view(request):
+# 渲染登录页面
+def index_view(request):
     return render(request, 'login.html')  # 渲染页面
+
+
+# 处理登录功能
+def login_view(request):
+    # 接收表单的请求参数
+    uname = request.GET.get('uname', '')  # 这里的uname表示网页表单中name为uname的input，逗号后面的表示为空
+    pwd = request.GET.get('pwd', '') # 这里的pwd表示网页表单中name为pwd的input，逗号后面的表示为空
+    # 判断
+    if uname == 'zhangsan' and pwd == '123':
+        return HttpResponse('登陆成功！')
+    return HttpResponse('登陆失败！')
