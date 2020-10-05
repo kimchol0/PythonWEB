a8934de308bee2d7d3f3aa038a8c8aed055293e8
KimChol <kimchol2ece@outlook.com>
2020-10-05 20:19:10 +0800

    优化注册功能代码

--- PythonWEB/settings.py
+++ PythonWEB/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = '54q%r9+s@6inycj(-#4-37gyr$m6c79*@pv7c+l50(kucb=k-7'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -44,7 +42,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',  # 此项若关闭，则可以防止403错误
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -71,7 +69,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'PythonWEB.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
@@ -81,7 +78,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -101,7 +97,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -114,7 +109,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
--- stu/views.py
+++ stu/views.py
@@ -1,27 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
 # Create your views here.
 # 渲染登录页面
-from stu.models import Student
+from .models import *
 
 
 def index_view(request):
-    # 获取当前请求方式（GET或POST）
-    m = request.method
-    if m == 'GET':
+    # 根据不同的请求方式处理不同的业务需求
+    if request.method == 'GET':
         return render(request, 'register.html')
     else:
-        # 获取请求参数
+        # 接收请求参数
         uname = request.POST.get('uname', '')
         pwd = request.POST.get('pwd', '')
-        # 判断
+        # 非空判断
         if uname and pwd:
             # 创建模型对象
-            stu = Student(sname=uname,spwd=pwd)
+            stu = Student(sname=uname, spwd=pwd)
             # 插入数据库
             stu.save()
+            # 页面响应
             return HttpResponse('注册成功')
         return HttpResponse('注册失败')
 
