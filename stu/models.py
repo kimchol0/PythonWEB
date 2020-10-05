14230cc6aa7cd31b41de554da9e3ef0d280dd02e
KimChol <kimchol2ece@outlook.com>
2020-10-05 17:01:20 +0800

    对0cca63f的补充

--- stu/models.py
+++ stu/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
 
-class Student(models.Model):
-    sname = models.CharField(max_length=30, unique=True)
+class Student(models.Model):  # 必须继承模型类，否则就是一个普通的类。否则无法对数据库进行操作
+    sname = models.CharField(max_length=30, unique=True)  # sname相当于数据库中的字段
     spwd = models.CharField(max_length=30)
-
