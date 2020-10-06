e172d1bc25baf56b83ab246978345f49db5902c7
KimChol <kimchol2ece@outlook.com>
2020-10-06 17:15:49 +0800

    后台管理admin操作数据库及配置文件和显示样式修改

--- stu/admin.py
+++ stu/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Student
 
 # Register your models here.
+admin.site.register(Student)
+# 然后在CMD中创建超级用户 python manage.py createsuperuser输入用户名邮箱密码 admin 1@1.com 123
--- stu/models.py
+++ stu/models.py
@@ -6,3 +6,6 @@ from django.db import models
 class Student(models.Model):  # 必须继承模型类，否则就是一个普通的类。否则无法对数据库进行操作
     sname = models.CharField(max_length=30, unique=True)  # sname相当于数据库中的字段
     spwd = models.CharField(max_length=30)
+
+    def __str__(self):
+        return u'Student:%s' % self.sname  # admin管理页面显示Student:对应的姓名，而非显示Student Object
