b6fc0019bde87d1d7876ef5e9532b84fdfb1b5df
KimChol <kimchol2ece@outlook.com>
2020-10-14 22:07:05 +0800

    创建数据库表（一对一）

--- stu/models.py
+++ stu/models.py
@@ -22,6 +22,7 @@ class Post(models.Model):
     isdelete = models.BooleanField(default=False)
     access_count = models.PositiveIntegerField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
+    file = models.ImageField(upload_to='upload/images/')  # 如果上传文件为非图片则改为FileField
 
     def __str__(self):
         return u'Post:%s,%s' % (self.title, self.access_count)
@@ -29,5 +30,21 @@ class Post(models.Model):
     class Meta:
         db_table = 't_post'
 
+
 # python manage.py makemigrations stu
 # python manage.py migrate
+
+class Student(models.Model):
+    sno = models.AutoField(primary_key=True)
+    sname = models.CharField(max_length=30)
+
+    def __str__(self):
+        return u'Student:%s' % self.sname
+
+
+class SCard(models.Model):
+    student = models.OneToOneField(Student, primary_key=True, on_delete=models.CASCADE)
+    major = models.CharField(max_length=30)
+
+    def __str__(self):
+        return u'Scard:%s' % self.major
