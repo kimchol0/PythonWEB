18c26536e5362bda4120fced3ed592607b8b865f
KimChol <kimchol2ece@outlook.com>
2020-10-15 22:38:56 +0800

    去掉多余的模型文件内容，创建新的迁移文件。

--- stu/admin.py
+++ stu/admin.py
@@ -4,4 +4,3 @@ from .models import *
 # Register your models here.
 admin.site.register(Student)
 # 然后在CMD中创建超级用户 python manage.py createsuperuser输入用户名邮箱密码 admin 1@1.com 123
-admin.site.register(Post)
--- stu/models.py
+++ stu/models.py
@@ -1,40 +1,20 @@
 from django.db import models
 
 
-# 帖子模型类
-class Post(models.Model):
-    pid = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=10, unique=True)
-    context = models.TextField()
-    create = models.DateTimeField(auto_now_add=True)
-    modified = models.DateTimeField(auto_now=True)
-    email = models.EmailField()
-    isdelete = models.BooleanField(default=False)
-    access_count = models.PositiveIntegerField()
-    price = models.DecimalField(max_digits=5, decimal_places=2)
-    file = models.ImageField(upload_to='upload/images/')  # 如果上传文件为非图片则改为FileField
-
-    def __str__(self):
-        return u'Post:%s,%s' % (self.title, self.access_count)
-
-    class Meta:
-        db_table = 't_post'
-
-
 # python manage.py makemigrations stu
 # python manage.py migrate
 
+# 班级主表（一）
+class Clazz(models.Model):
+    cno = models.AutoField(primary_key=True)
+    cname = models.CharField(max_length=30)
+
+
+# 学生表（从表）（多）
 class Student(models.Model):
     sno = models.AutoField(primary_key=True)
     sname = models.CharField(max_length=30)
+    cno = models.ForeignKey(Clazz, on_delete=models.CASCADE)
 
     def __str__(self):
         return u'Student:%s' % self.sname
-
-
-class Scard(models.Model):
-    st = models.OneToOneField(Student, primary_key=True, on_delete=models.CASCADE)
-    major = models.CharField(max_length=30)
-
-    def __str__(self):
-        return u'Scard:%s' % self.major
