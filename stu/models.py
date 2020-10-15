e2a1419d7816f7dae167ce259330bfe2c1e8b7b9
KimChol <kimchol2ece@outlook.com>
2020-10-15 22:04:30 +0800

    一对一查询

--- stu/models.py
+++ stu/models.py
@@ -1,16 +1,6 @@
 from django.db import models
 
 
-# Create your models here.
-
-class Student(models.Model):  # 必须继承模型类，否则就是一个普通的类。否则无法对数据库进行操作
-    sname = models.CharField(max_length=30, unique=True)  # sname相当于数据库中的字段
-    spwd = models.CharField(max_length=30)
-
-    def __str__(self):
-        return u'Student:%s' % self.sname  # admin管理页面显示Student:对应的姓名，而非显示Student Object
-
-
 # 帖子模型类
 class Post(models.Model):
     pid = models.AutoField(primary_key=True)
@@ -42,8 +32,8 @@ class Student(models.Model):
         return u'Student:%s' % self.sname
 
 
-class SCard(models.Model):
-    student = models.OneToOneField(Student, primary_key=True, on_delete=models.CASCADE)
+class Scard(models.Model):
+    st = models.OneToOneField(Student, primary_key=True, on_delete=models.CASCADE)
     major = models.CharField(max_length=30)
 
     def __str__(self):
