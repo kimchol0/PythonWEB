f573709c01bb16325e149c5d9f1308ee51b57eb4
KimChol <kimchol2ece@outlook.com>
2020-10-16 21:31:29 +0800

    多对多查询

--- stu/models.py
+++ stu/models.py
@@ -20,7 +20,25 @@ class Student(models.Model):
         return u'Student:%s' % self.sname
 
 
+class Course(models.Model):
+    course_no = models.AutoField(primary_key=True)
+    course_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return u'Course:%s' % self.course_name
+
+
+class Teacher(models.Model):
+    tno = models.AutoField(primary_key=True)
+    tname = models.CharField(max_length=30)
+    cour = models.ManyToManyField(Course)
+
+    def __str__(self):
+        return u'Teacher:%s' % self.tname
+
+
 # insertData('B203HTML5班','zhangjie','xiena')
+# 以下内容在Python Console中输入，不要忘记from stu.models import *
 def insertData(clsname, *snames):
     try:
         cls = Clazz.objects.get(cname=clsname)
@@ -31,3 +49,20 @@ def insertData(clsname, *snames):
                 stu = Student.objects.get(sname=sn)
             except Student.DoesNotExist:
                 Student.objects.create(snamme=sn, cno=cls)
+
+
+# insertData2('wangwu','Oracle','MySql')
+# 以下内容在Python Console中输入，不要忘记from stu.models import *
+def insertData2(tname, *coursenames):
+    try:
+        t = Teacher.objects.get(tname=tname)
+    except Teacher.DoesNotExist:
+        t = Teacher.objects.create(tname=tname)
+        courseList = []
+    for cn in coursenames:
+        try:
+            cou = Course.objects.get(course_name=cn)
+        except Course.DoesNotExist:
+            cou = Course.objects.create(course_name=cn)
+            courseList.append(cou)
+        t.cour.add(*courseList)
